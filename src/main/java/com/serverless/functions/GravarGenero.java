43d30d5102dbe98238d8a1826b48fa266383640d
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2021-06-02 11:24:40 -0300

    Criar função lambda para salvar um novo genero

---
+++ src/main/java/com/serverless/functions/GravarGenero.java
@@ -0,0 +1,36 @@
+package com.serverless.functions;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.serverless.ApiGatewayResponse;
+import com.serverless.Response;
+import com.serverless.services.GeneroService;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.Collections;
+import java.util.Map;
+
+public class GravarGenero implements RequestHandler<Map<String, Object>, ApiGatewayResponse> {
+
+    private GeneroService generoService;
+    private static final Logger LOG = LogManager.getLogger(GravarGenero.class);
+
+    @Override
+    public ApiGatewayResponse handleRequest(Map<String, Object> stringObjectMap, Context context) {
+        LOG.info("Gravando o novo genero no dynamoDB");
+
+        this.generoService = new GeneroService();
+        this.generoService.cadastrarNovoGeneroDeLivro(stringObjectMap.get("nome").toString());
+
+        LOG.info("Gravação efetuada com sucesso!");
+
+        Response response = new Response("Gravação efetuada com sucesso!", stringObjectMap);
+
+        return ApiGatewayResponse.builder()
+                .setStatusCode(201)
+                .setHeaders(Collections.singletonMap("X-Powered-By", "AWS Lambda & serverless"))
+                .setObjectBody(response)
+                .build();
+    }
+}
