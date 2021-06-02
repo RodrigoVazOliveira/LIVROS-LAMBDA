6587ce5a0f5df065cd23251ded72eff3866f7d92
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2021-06-02 10:49:13 -0300

    Criar método para salvar um novo genero

--- src/main/java/com/serverless/services/GeneroService.java
+++ src/main/java/com/serverless/services/GeneroService.java
@@ -1,4 +1,28 @@
 package com.serverless.services;
 
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
+import com.serverless.configurations.DynamoDBConfiguration;
+import com.serverless.models.Genero;
+
 public class GeneroService {
+
+    private DynamoDBMapper dynamoDBMapper;
+    private DynamoDBConfiguration dynamoDBConfiguration;
+
+    public void cadastrarNovoGeneroDeLivro(String nome) {
+        gerarMapper();
+        Genero genero = new Genero();
+        genero.setNome(nome);
+        this.dynamoDBMapper.save(genero);
+    }
+
+    public void gerarMapper() {
+        if (this.dynamoDBConfiguration != null) {
+            this.dynamoDBConfiguration = new DynamoDBConfiguration();
+            this.dynamoDBConfiguration.build();
+            this.dynamoDBConfiguration.buildDynamoDBMapper();
+        }
+
+        this.dynamoDBMapper = this.dynamoDBConfiguration.getDynamoDBMapper();
+    }
 }
