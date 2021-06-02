6a8d6dab0c18c45a61cae23746bc482b66b5ac74
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2021-06-02 10:39:54 -0300

    Criar configuração do dynamoDBMapper

--- src/main/java/com/serverless/configurations/DynamoDBConfiguration.java
+++ src/main/java/com/serverless/configurations/DynamoDBConfiguration.java
@@ -2,10 +2,12 @@ package com.serverless.configurations;
 
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClient;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
 
 public class DynamoDBConfiguration {
 
     private AmazonDynamoDB dynamoDB;
+    private DynamoDBMapper dynamoDBMapper;
 
     public void build() {
         this.dynamoDB = AmazonDynamoDBClient.builder().build();
@@ -13,5 +15,13 @@ public class DynamoDBConfiguration {
 
     public AmazonDynamoDB getDynamoDB() {
         return dynamoDB;
+    }
+
+    public void buildDynamoDBMapper() {
+        this.dynamoDBMapper = new DynamoDBMapper(this.dynamoDB);
+    }
+
+    public DynamoDBMapper getDynamoDBMapper() {
+        return dynamoDBMapper;
     }
 }
