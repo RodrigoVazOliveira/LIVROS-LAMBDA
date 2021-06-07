b6deeb288909237b07327437d213375a00ddd5e7
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2021-06-07 14:27:37 -0300

    Criar teste caso não localize algum registro

--- src/tests/com/serverless/tests/services/GeneroServiceTest.java
+++ src/tests/com/serverless/tests/services/GeneroServiceTest.java
@@ -5,6 +5,7 @@ import com.serverless.configurations.DynamoDBConfiguration;
 import com.serverless.models.Genero;
 import com.serverless.services.GeneroService;
 import org.junit.Assert;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -75,5 +76,17 @@ public class GeneroServiceTest {
         this.testeGenero.setId("IdTEst");
         this.generoService.atualizarGenero(this.testeGenero);
         verify(dynamoDBMapper).save(any());
+    }
+
+    @Test
+    public void testarAtualizacaoDeGeneroError() {
+        lenient().when(this.dynamoDBMapper.query(eq(Genero.class), any(DynamoDBQueryExpression.class))).thenReturn(this.mockListBuscaPorId);
+        this.testeGenero.setId("IdTEst");
+
+        RuntimeException ex =  Assertions.assertThrows(RuntimeException.class, () -> {
+            this.generoService.atualizarGenero(this.testeGenero);
+        });
+
+        Assert.assertEquals(ex.getMessage(), "Não foi localziado nenhum genero com id IdTEst");
     }
 }
