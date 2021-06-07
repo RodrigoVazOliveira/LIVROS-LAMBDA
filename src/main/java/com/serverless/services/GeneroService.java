10529f8543b840516cad2eaf5eec49cd0a5ceac4
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2021-06-07 12:25:36 -0300

    Criar método para atualizar o genero

--- src/main/java/com/serverless/services/GeneroService.java
+++ src/main/java/com/serverless/services/GeneroService.java
@@ -1,12 +1,16 @@
 package com.serverless.services;
 
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBQueryExpression;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBScanExpression;
+import com.amazonaws.services.dynamodbv2.model.AttributeValue;
 import com.serverless.configurations.DynamoDBConfiguration;
 import com.serverless.models.Genero;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 public class GeneroService {
@@ -34,5 +38,31 @@ public class GeneroService {
     public Iterable<Genero> obterTodosGeneros() {
         gerarMapper();
         return this.dynamoDBMapper.scan(Genero.class, new DynamoDBScanExpression()).stream().collect(Collectors.toList());
+    }
+
+    public Genero buscarGeneroPorId(String id) {
+        gerarMapper();
+        Map<String, AttributeValue> valorABuscar = new HashMap<>();
+        valorABuscar.put("valorUm", new AttributeValue().withS(id));
+
+        DynamoDBQueryExpression<Genero> query = new DynamoDBQueryExpression<>();
+        query.withConditionalOperator("Id = :valorUm");
+        query.withExpressionAttributeValues(valorABuscar);
+
+        List<Genero> resultado = this.dynamoDBMapper.query(Genero.class, query);
+
+        if (resultado.size() == 0 || resultado == null) {
+            throw new RuntimeException("Não foi localziado nenhum genero com id " + id);
+        }
+
+        return resultado.get(0);
+    }
+
+    public void atualizarGenero(Genero generoAtualizar) {
+        gerarMapper();
+        Genero genero = this.buscarGeneroPorId(generoAtualizar.getId());
+        genero.setNome(generoAtualizar.getNome());
+
+        this.dynamoDBMapper.save(genero);
     }
 }
