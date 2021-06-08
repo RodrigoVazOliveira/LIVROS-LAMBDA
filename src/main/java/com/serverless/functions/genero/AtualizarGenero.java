4f90d3dd18629b314521273d783dfa888539a7b4
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2021-06-08 09:05:54 -0300

    Criar método para atualizar o genero com a lambda

--- src/main/java/com/serverless/functions/genero/AtualizarGenero.java
+++ src/main/java/com/serverless/functions/genero/AtualizarGenero.java
@@ -3,16 +3,30 @@ package com.serverless.functions.genero;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.serverless.ApiGatewayResponse;
+import com.serverless.Response;
+import com.serverless.models.Genero;
 import com.serverless.services.GeneroService;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
+import java.util.Collections;
 import java.util.Map;
 
 public class AtualizarGenero implements RequestHandler<Map<String, Object>, ApiGatewayResponse> {
 
     private final GeneroService generoService = new GeneroService();
+    private final Genero genero = new Genero();
+    private static final Logger LOG = LogManager.getLogger(AtualizarGenero.class);
 
     @Override
     public ApiGatewayResponse handleRequest(Map<String, Object> stringObjectMap, Context context) {
-        return null;
+        LOG.info("Capturando as informações enviadas.");
+
+        this.genero.setId(stringObjectMap.get("Id").toString());
+        this.genero.setNome(stringObjectMap.get("nome").toString());
+
+        LOG.info("Iniciando o processo de atualização no DynamoDB");
+
+        return verificarResposta(stringObjectMap);
     }
 }
--- src/main/java/com/serverless/services/GeneroService.java
+++ src/main/java/com/serverless/services/GeneroService.java
@@ -7,7 +7,6 @@ import com.amazonaws.services.dynamodbv2.model.AttributeValue;
 import com.serverless.configurations.DynamoDBConfiguration;
 import com.serverless.models.Genero;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -63,6 +62,6 @@ public class GeneroService {
         Genero genero = this.buscarGeneroPorId(generoAtualizar.getId());
         genero.setNome(generoAtualizar.getNome());
 
-        this.dynamoDBMapper.save(genero);
+        this.dynamoDBMapper.save(geneo);
     }
 }
