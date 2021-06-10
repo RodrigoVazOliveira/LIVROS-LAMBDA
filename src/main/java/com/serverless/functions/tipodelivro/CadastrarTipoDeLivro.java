8dc1e6d8be8954ce987d7e0b9aa13e3145905602
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2021-06-10 16:53:13 -0300

    Criar o método da lambda

--- src/main/java/com/serverless/functions/tipodelivro/CadastrarTipoDeLivro.java
+++ src/main/java/com/serverless/functions/tipodelivro/CadastrarTipoDeLivro.java
@@ -3,10 +3,13 @@ package com.serverless.functions.tipodelivro;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.serverless.ApiGatewayResponse;
+import com.serverless.Response;
+import com.serverless.models.TipoDeLivro;
 import com.serverless.services.TipoDeLivroService;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.util.Collections;
 import java.util.Map;
 
 public class CadastrarTipoDeLivro implements RequestHandler<Map<String, Object>, ApiGatewayResponse> {
@@ -24,6 +27,21 @@ public class CadastrarTipoDeLivro implements RequestHandler<Map<String, Object>,
 
     @Override
     public ApiGatewayResponse handleRequest(Map<String, Object> stringObjectMap, Context context) {
-        return null;
+        LOG.info("Inicio de gravação dos dados do tipo de livro");
+        LOG.info("Resgatando dados enviados");
+
+        TipoDeLivro tipoDeLivro = new TipoDeLivro();
+        tipoDeLivro.setNome(stringObjectMap.get("nome").toString());
+
+        LOG.info("Gravando os dados no dynamoDB");
+        this.tipoDeLivroService.cadastrarNovoTipoDeLivro(tipoDeLivro);
+
+        return ApiGatewayResponse.builder()
+                .setHeaders(Collections.singletonMap("X-Powered-By", "AWS Lambda & serverless"))
+                .setObjectBody(
+                        new Response("O tipo de livro " + tipoDeLivro + " foi gravado com sucesso!"
+                                , stringObjectMap))
+                .setStatusCode(201)
+                .build();
     }
 }
