c1a5ee1abe7aae8b0724645368a0798f2da48788
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2021-06-11 15:01:15 -0300

    Criar teste caso erro de dados informados

--- src/tests/com/serverless/tests/functions/tipodelivro/AtualizarTipoDeLivroTest.java
+++ src/tests/com/serverless/tests/functions/tipodelivro/AtualizarTipoDeLivroTest.java
@@ -6,7 +6,8 @@ import com.serverless.Response;
 import com.serverless.functions.tipodelivro.AtualizarTipoDeLivro;
 import com.serverless.models.TipoDeLivro;
 import com.serverless.services.TipoDeLivroService;
-import org.junit.Assert;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -17,8 +18,7 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
-import static org.mockito.Mockito.any;
-import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.*;
 
 ;
 
@@ -38,6 +38,7 @@ public class AtualizarTipoDeLivroTest {
 
     private TipoDeLivro tipoDeLivro;
 
+    @BeforeEach
     public void setup() {
         this.tipoDeLivro = new TipoDeLivro();
         this.tipoDeLivro.setId("1");
@@ -47,7 +48,7 @@ public class AtualizarTipoDeLivroTest {
     }
 
     @Test
-    public void testarHandlerRequest() {
+    public void testarHandlerRequestComSucesso() {
         doNothing().when(this.tipoDeLivroService).atualizarTipoDeLivro(any());
         input.put("id", "1");
         input.put("nome", "Capa dura");
@@ -60,8 +61,28 @@ public class AtualizarTipoDeLivroTest {
                 .setHeaders(Collections.singletonMap("X-Powered-By", "AWS Lambda & serverless"))
                 .build();
 
-        Assert.assertEquals(respostaEsperada.getStatusCode(), respostaTest.getStatusCode());
-        Assert.assertEquals(respostaEsperada.getBody(), respostaTest.getBody());
-        Assert.assertEquals(respostaEsperada.getHeaders(), respostaTest.getHeaders());
+        Assertions.assertEquals(respostaEsperada.getStatusCode(), respostaTest.getStatusCode());
+        Assertions.assertEquals(respostaEsperada.getBody(), respostaTest.getBody());
+        Assertions.assertEquals(respostaEsperada.getHeaders(), respostaTest.getHeaders());
+    }
+
+    @Test
+    public void testarHandlerRequestComError() {
+        doThrow(new RuntimeException("não existe um tipo de livro com id 1"))
+                .when(this.tipoDeLivroService).atualizarTipoDeLivro(any());
+        input.put("id", "1");
+        input.put("nome", "Capa dura");
+
+        ApiGatewayResponse respostaTest = this.atualizarTipoDeLivro.handleRequest(this.input, this.context);
+
+        ApiGatewayResponse respostaEsperada = ApiGatewayResponse.builder()
+                .setStatusCode(400)
+                .setObjectBody(new Response("não existe um tipo de livro com id 1", this.input))
+                .setHeaders(Collections.singletonMap("X-Powered-By", "AWS Lambda & serverless"))
+                .build();
+
+        Assertions.assertEquals(respostaEsperada.getStatusCode(), respostaTest.getStatusCode());
+        Assertions.assertEquals(respostaEsperada.getBody(), respostaTest.getBody());
+        Assertions.assertEquals(respostaEsperada.getHeaders(), respostaTest.getHeaders());
     }
 }
