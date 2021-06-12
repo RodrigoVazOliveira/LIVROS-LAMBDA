e88565e03ee4e6f5a5cf7f4f77de64568bcd8bf0
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2021-06-12 17:36:22 -0300

    Tratar dados ao deletar tipo de livro

--- src/main/java/com/serverless/functions/tipodelivro/DeleteTipoDeLivro.java
+++ src/main/java/com/serverless/functions/tipodelivro/DeleteTipoDeLivro.java
@@ -2,12 +2,15 @@ package com.serverless.functions.tipodelivro;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.serverless.ApiGatewayResponse;
 import com.serverless.Response;
 import com.serverless.services.TipoDeLivroService;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.io.IOException;
 import java.util.Collections;
 import java.util.Map;
 
@@ -15,6 +18,7 @@ public class DeleteTipoDeLivro implements RequestHandler<Map<String, Object>, Ap
 
     private TipoDeLivroService tipoDeLivroService;
     private static final Logger LOG = LogManager.getLogger(DeleteTipoDeLivro.class);
+    private String id;
 
     public DeleteTipoDeLivro() {
         this.tipoDeLivroService = new TipoDeLivroService();
@@ -27,10 +31,10 @@ public class DeleteTipoDeLivro implements RequestHandler<Map<String, Object>, Ap
     @Override
     public ApiGatewayResponse handleRequest(Map<String, Object> input, Context context) {
         LOG.info("Iniciando o lambda e tratando os dados enviados");
-        String id = input.get("id").toString();
+        getData();
         LOG.info("Iniciando serviço do tipo do livro para deletar o tipo de livro no ndynamoDB");
         try {
-            this.tipoDeLivroService.deleteTipoDeLivro(id);
+            this.tipoDeLivroService.deleteTipoDeLivro(this.id);
             return ApiGatewayResponse.builder()
                     .setStatusCode(204)
                     .setObjectBody(new Response("tipo de livro deletado!", input))
@@ -43,5 +47,16 @@ public class DeleteTipoDeLivro implements RequestHandler<Map<String, Object>, Ap
                     .setHeaders(Collections.singletonMap("X-Powered-By", "AWS Lambda & serverless"))
                     .build();
         }
+    }
+
+    private void getData(Map<String, Object> input) {
+        ObjectMapper objectMapper = new ObjectMapper();
+        try {
+            JsonNode body = objectMapper.readTree((String) input.get("body"));
+            this.id = body.get("id").asText();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 }
