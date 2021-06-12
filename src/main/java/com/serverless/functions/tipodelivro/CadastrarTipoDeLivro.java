1a58d0daec07a52a9cc0d15348820c2168716448
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2021-06-12 17:27:15 -0300

    Tratando dados do gravar tipo de livro

--- src/main/java/com/serverless/functions/tipodelivro/CadastrarTipoDeLivro.java
+++ src/main/java/com/serverless/functions/tipodelivro/CadastrarTipoDeLivro.java
@@ -2,6 +2,8 @@ package com.serverless.functions.tipodelivro;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.serverless.ApiGatewayResponse;
 import com.serverless.Response;
 import com.serverless.models.TipoDeLivro;
@@ -9,6 +11,7 @@ import com.serverless.services.TipoDeLivroService;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.io.IOException;
 import java.util.Collections;
 import java.util.Map;
 
@@ -16,6 +19,7 @@ public class CadastrarTipoDeLivro implements RequestHandler<Map<String, Object>,
 
     private static final Logger LOG = LogManager.getLogger(CadastrarTipoDeLivro.class);
     private TipoDeLivroService tipoDeLivroService;
+    private TipoDeLivro tipoDeLivro;
 
     public CadastrarTipoDeLivro() {
         this.tipoDeLivroService = new TipoDeLivroService();
@@ -30,11 +34,10 @@ public class CadastrarTipoDeLivro implements RequestHandler<Map<String, Object>,
         LOG.info("Inicio de gravação dos dados do tipo de livro");
         LOG.info("Resgatando dados enviados");
 
-        TipoDeLivro tipoDeLivro = new TipoDeLivro();
-        tipoDeLivro.setNome(input.get("nome").toString());
+        getData(input);
 
         LOG.info("Gravando os dados no dynamoDB");
-        this.tipoDeLivroService.cadastrarNovoTipoDeLivro(tipoDeLivro);
+        this.tipoDeLivroService.cadastrarNovoTipoDeLivro(this.tipoDeLivro);
 
         return ApiGatewayResponse.builder()
                 .setHeaders(Collections.singletonMap("X-Powered-By", "AWS Lambda & serverless"))
@@ -43,5 +46,16 @@ public class CadastrarTipoDeLivro implements RequestHandler<Map<String, Object>,
                                 , input))
                 .setStatusCode(201)
                 .build();
+    }
+
+    private void getData(Map<String, Object> input) {
+        ObjectMapper objectMapper = new ObjectMapper();
+        this.tipoDeLivro = new TipoDeLivro();
+        try {
+            JsonNode body = objectMapper.readTree( (String) input.get("body") );
+            this.tipoDeLivro.setNome(body.get("nome").asText());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
