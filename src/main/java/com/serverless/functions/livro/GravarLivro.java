b0f6416ca80af324cbe8b3e1aeb6b9bf141f130d
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2021-06-14 18:21:46 -0300

    Criar método para pega o corpo de entrada

--- src/main/java/com/serverless/functions/livro/GravarLivro.java
+++ src/main/java/com/serverless/functions/livro/GravarLivro.java
@@ -2,18 +2,49 @@ package com.serverless.functions.livro;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.serverless.ApiGatewayResponse;
+import com.serverless.Response;
+import com.serverless.helper.GetInput;
+import com.serverless.helper.ObjectMapperProxy;
+import com.serverless.models.Livro;
+import com.serverless.services.LivroService;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.util.Collections;
 import java.util.Map;
 
 public class GravarLivro implements RequestHandler<Map<String, Object>, ApiGatewayResponse> {
 
     private static final Logger LOG = LogManager.getLogger(GravarLivro.class);
+    private LivroService livroService;
+    private Livro livro;
+    private Map<String, Object> input;
+    private ApiGatewayResponse response;
+    private final ObjectMapper objectMapper = ObjectMapperProxy.getObjectMapper();
+
+    public GravarLivro() {
+        this.livroService = new LivroService();
+    }
+
+    public GravarLivro(LivroService livroService) {
+        this.livroService = livroService;
+    }
 
     @Override
-    public ApiGatewayResponse handleRequest(Map<String, Object> stringObjectMap, Context context) {
-        return null;
+    public ApiGatewayResponse handleRequest(Map<String, Object> input, Context context) {
+        LOG.info("Iniciando processo de gravação do livro");
+        this.input = input;
+        getData();
+        gravarNovoLivroNoDynamoDB();
+        return this.response;
+    }
+
+    private void getData() {
+        LOG.info("Tratando os dados");
+        JsonNode body = GetInput.getBody(this.input);
+        createLivro(body);
     }
 }
--- src/main/java/com/serverless/services/LivroService.java
+++ src/main/java/com/serverless/services/LivroService.java
@@ -2,6 +2,7 @@ package com.serverless.services;
 
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
 import com.serverless.configurations.DynamoDBConfiguration;
+import com.serverless.models.Livro;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -9,11 +10,17 @@ public class LivroService {
 
     private DynamoDBMapper dynamoDBMapper = DynamoDBConfiguration.build();
     private static final Logger LOG = LogManager.getLogger(LivroService.class);
+    private GeneroService generoService;
+    private TipoDeLivroService tipoDeLivroService;
 
     public LivroService() {
     }
 
     public LivroService(DynamoDBMapper dynamoDBMapper) {
         this.dynamoDBMapper = dynamoDBMapper;
+    }
+
+    public void gravarNovoLivro(Livro livro) {
+        this.dynamoDBMapper.save(livro);
     }
 }
