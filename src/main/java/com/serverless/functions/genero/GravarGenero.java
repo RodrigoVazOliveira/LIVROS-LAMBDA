e6a9f8716a4a816651bc53798ab1a2a29caa84e0
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2021-06-14 10:37:19 -0300

    Refatorado classe gravar genero

--- src/main/java/com/serverless/functions/genero/GravarGenero.java
+++ src/main/java/com/serverless/functions/genero/GravarGenero.java
@@ -3,14 +3,13 @@ package com.serverless.functions.genero;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.serverless.ApiGatewayResponse;
 import com.serverless.Response;
+import com.serverless.helper.GetInput;
 import com.serverless.services.GeneroService;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import java.io.IOException;
 import java.util.Collections;
 import java.util.Map;
 
@@ -31,30 +30,19 @@ public class GravarGenero implements RequestHandler<Map<String, Object>, ApiGate
     @Override
     public ApiGatewayResponse handleRequest(Map<String, Object> input, Context context) {
         LOG.info("Gravando o novo genero no dynamoDB");
-
         getData(input);
-
         this.generoService.cadastrarNovoGeneroDeLivro(this.nome);
-
         LOG.info("Gravação efetuada com sucesso!");
-
-        Response response = new Response("Gravação efetuada com sucesso!", input);
 
         return ApiGatewayResponse.builder()
                 .setStatusCode(201)
                 .setHeaders(Collections.singletonMap("X-Powered-By", "AWS Lambda & serverless"))
-                .setObjectBody(response)
+                .setObjectBody(new Response("Gravação efetuada com sucesso!", input))
                 .build();
     }
 
     private void getData(Map<String, Object> input) {
-        ObjectMapper objectMapper = new ObjectMapper();
-
-        try {
-            JsonNode body = objectMapper.readTree( (String) input.get("body") );
-            this.nome = body.get("nome").asText();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        JsonNode body = GetInput.getBody(input);
+        this.nome = body.get("nome").asText();
     }
 }
