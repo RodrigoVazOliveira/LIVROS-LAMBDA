27d15f1f38b84cab0ab4c102bd30e25b8547b950
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2021-06-14 16:13:24 -0300

    configurar o serviço de livro

--- src/main/java/com/serverless/services/LivroService.java
+++ src/main/java/com/serverless/services/LivroService.java
@@ -1,4 +1,19 @@
 package com.serverless.services;
 
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
+import com.serverless.configurations.DynamoDBConfiguration;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class LivroService {
+
+    private DynamoDBMapper dynamoDBMapper = DynamoDBConfiguration.build();
+    private static final Logger LOG = LogManager.getLogger(LivroService.class);
+
+    public LivroService() {
+    }
+
+    public LivroService(DynamoDBMapper dynamoDBMapper) {
+        this.dynamoDBMapper = dynamoDBMapper;
+    }
 }
