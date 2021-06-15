6ef25369327f903879f996c92b4db69df9420926
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2021-06-15 15:05:35 -0300

    criar construtores

--- src/main/java/com/serverless/functions/livro/AtualizarLivro.java
+++ src/main/java/com/serverless/functions/livro/AtualizarLivro.java
@@ -3,10 +3,22 @@ package com.serverless.functions.livro;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.serverless.ApiGatewayResponse;
+import com.serverless.services.LivroService;
 
 import java.util.Map;
 
 public class AtualizarLivro implements RequestHandler<Map<String, Object>, ApiGatewayResponse> {
+
+    private LivroService livroService;
+
+    public AtualizarLivro() {
+        this.livroService = new LivroService();
+    }
+
+    public AtualizarLivro(LivroService livroService) {
+        this.livroService = livroService;
+    }
+
     @Override
     public ApiGatewayResponse handleRequest(Map<String, Object> input, Context context) {
         return null;
