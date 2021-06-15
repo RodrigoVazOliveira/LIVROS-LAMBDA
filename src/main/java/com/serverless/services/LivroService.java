f091172b85c251b3802a2f33705a57025c653368
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2021-06-15 13:18:26 -0300

    Criar método para obter todos os livros

--- src/main/java/com/serverless/services/LivroService.java
+++ src/main/java/com/serverless/services/LivroService.java
@@ -1,17 +1,19 @@
 package com.serverless.services;
 
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBScanExpression;
 import com.serverless.configurations.DynamoDBConfiguration;
 import com.serverless.models.Livro;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class LivroService {
 
     private DynamoDBMapper dynamoDBMapper = DynamoDBConfiguration.build();
     private static final Logger LOG = LogManager.getLogger(LivroService.class);
-    private GeneroService generoService;
-    private TipoDeLivroService tipoDeLivroService;
 
     public LivroService() {
     }
@@ -22,5 +24,11 @@ public class LivroService {
 
     public void gravarNovoLivro(Livro livro) {
         this.dynamoDBMapper.save(livro);
+    }
+
+    public List<Livro> obterTodosOsLivros() throws RuntimeException {
+        LOG.info("Acessando o dynamoDB");
+        return this.dynamoDBMapper.scan(Livro.class, new DynamoDBScanExpression())
+                .stream().collect(Collectors.toList());
     }
 }
