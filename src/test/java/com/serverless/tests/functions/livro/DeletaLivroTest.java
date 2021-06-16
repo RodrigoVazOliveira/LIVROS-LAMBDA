d53753ad4b3dce80dcaa1c9909f883c29f295a87
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2021-06-16 17:35:19 -0300

    Criar teste de deletar livro'

---
+++ src/test/java/com/serverless/tests/functions/livro/DeletaLivroTest.java
@@ -0,0 +1,9 @@
+package com.serverless.tests.functions.livro;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class DeletaLivroTest {
+
+}
